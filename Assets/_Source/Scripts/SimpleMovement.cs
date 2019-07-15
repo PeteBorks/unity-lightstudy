c5bdf025ecb645d875ca9b47a275dc2236ac58e8
Pedro Borges <pedroalonsoborges@hotmail.com>
2019-07-15 06:35:38 -0300

    Study 2 Development

---
+++ Assets/_Source/Scripts/SimpleMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5;
+
+    CharacterController cc;
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        cc.Move(movement * speed);
+    }
+
+}
---
+++ Assets/_Source/Shaders/BakeryHDRP_GBuffer.hlsl
@@ -0,0 +1,66 @@
+﻿#if SHADERPASS != SHADERPASS_GBUFFER
+#error SHADERPASS_is_not_correctly_define
+#endif
+
+#include "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/VertMesh.hlsl"
+#include "BakeryHDRP.hlsl"
+
+PackedVaryingsType Vert(AttributesMesh inputMesh)
+{
+    VaryingsType varyingsType;
+    varyingsType.vmesh = VertMesh(inputMesh);
+    return PackVaryingsType(varyingsType);
+}
+
+#ifdef TESSELLATION_ON
+
+PackedVaryingsToPS VertTesselation(VaryingsToDS input)
+{
+    VaryingsToPS output;
+    output.vmesh = VertMeshTesselation(input.vmesh);
+    return PackVaryingsToPS(output);
+}
+
+#include "Packages/com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/ShaderPass/TessellationShare.hlsl"
+
+#endif // TESSELLATION_ON
+
+void Frag(  PackedVaryingsToPS packedInput,
+            OUTPUT_GBUFFER(outGBuffer)
+            #ifdef _DEPTHOFFSET_ON
+            , out float outputDepth : SV_Depth
+            #endif
+            )
+{
+    FragInputs input = UnpackVaryingsMeshToFragInputs(packedInput.vmesh);
+
+    // input.positionSS is SV_Position
+    PositionInputs posInput = GetPositionInput(input.positionSS.xy, _ScreenSize.zw, input.positionSS.z, input.positionSS.w, input.positionRWS);
+
+#ifdef VARYINGS_NEED_POSITION_WS
+    float3 V = GetWorldSpaceNormalizeViewDir(input.positionRWS);
+#else
+    // Unused
+    float3 V = float3(1.0, 1.0, 1.0); // Avoid the division by 0
+#endif
+
+    SurfaceData surfaceData;
+    BuiltinData builtinData;
+    GetSurfaceAndBuiltinData(input, V, posInput, surfaceData, builtinData);
+
+#ifdef BAKERY_SH
+    if (bakeryLightmapMode == BAKERYMODE_SH)
+    {
+        float3 indirectSpecular = 0;
+        BakerySH(builtinData.bakeDiffuseLighting, indirectSpecular, input.texCoord1 * unity_LightmapST.xy + unity_LightmapST.zw, surfaceData.normalWS, 0, 0);
+        builtinData.bakeDiffuseLighting = max(builtinData.bakeDiffuseLighting * surfaceData.baseColor, 0);
+        builtinData.backBakeDiffuseLighting = builtinData.bakeDiffuseLighting;
+    }
+#endif
+
+    ENCODE_INTO_GBUFFER(surfaceData, builtinData, posInput.positionSS, outGBuffer);
+
+#ifdef _DEPTHOFFSET_ON
+    outputDepth = posInput.deviceDepth;
+#endif
+}
